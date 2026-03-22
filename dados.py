224b69970a8efad2714cce19803fff2dbc09043a
Giovanna Lima <giovannapslima.gpsl@gmail.com>
2026-03-22 15:23:46 -0300

    Update Excel file source and adjust plot limits

--- dados.py
+++ dados.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Lendo o arquivo excel
-dadosPlanilha = pd.read_excel('dadosenadefinal.xlsx')
+dadosPlanilha = pd.read_excel('dados.xlsx', engine='openpyxl')
 
 # Declarando as colunas e linhas
 colunas = dadosPlanilha.columns
@@ -20,24 +20,23 @@ def dadosGraficos():
         x = universidade['ANO'].astype(str)
         y = universidade['CONCEITO']
 
-        plt.ylim(0, 5)
+        plt.ylim(0, 5.5)
         plt.bar(x, y, color='#5177E6')
         plt.title(i, fontweight='bold')
         plt.xlabel("Ano")
         plt.ylabel("Conceito")
         plt.show()
 
-  
 #Comparar universidades em 2026
 def compararUniversidades():
     ano = dadosPlanilha[dadosPlanilha['ANO'] == 2026]
     x = ano['UNIVERSIDADE']
     y = ano['CONCEITO']
 
-    plt.title("Comparação de Conceitos das Universidades", fontweight='bold')
+    plt.title("Comparação de Conceito das Universidades", fontweight='bold')
     plt.xlabel("Universidade")
     plt.ylabel("Conceito")
-    plt.ylim(0, 5)
+    plt.ylim(0, 5.5)
     plt.bar(x, y, color='#5177E6')
     plt.xticks(rotation=45, ha='right', fontsize=8)
     plt.show()
